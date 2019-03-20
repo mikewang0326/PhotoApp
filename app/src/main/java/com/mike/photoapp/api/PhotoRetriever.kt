a3bb54a4db038b47de9bcf1551508a758c9270c5
MikeWang <hi.from.mikewang@gmail.com>
2019-03-20 21:20:43 +1300

    Add PhotoRetriever

---
+++ app/src/main/java/com/mike/photoapp/api/PhotoRetriever.kt
@@ -0,0 +1,24 @@
+package com.mike.photoapp.api
+
+import com.mike.photoapp.models.PhotoList
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PhotoRetriever {
+    private val service: PhotoAPI
+
+    init {
+        val retrofit =
+            Retrofit.Builder().baseUrl("https://pixabay.com/api/").addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        service = retrofit.create(PhotoAPI::class.java)
+    }
+
+    fun getPhotos(callback: Callback<PhotoList>) {
+        val call = service.getPhotos()
+        call.enqueue(callback)
+    }
+
+}
